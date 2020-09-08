75b723c6fd7a28ccd71945b618063aa5857097e9
Gabriel Gonçalves <GGGguimaraesgabriel@hotmail.com>
2020-09-08 14:04:56 -0300

    Generics part 1

---
+++ Learn-Swift/20. Generics.playground/Contents.swift
@@ -0,0 +1,105 @@
+//MARK: GENERICS
+
+//PROBLEM
+
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
+
+//the function only accepts int. And when it is double or string?
+
+func swapTwoStrings(_ a: inout String, _ b: inout String) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+func swapTwoDoubles(_ a: inout Double, _ b: inout Double) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+//same function
+
+//MARK: GENERIC FUNCTIONS
+
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+var someInt2 = 3
+var anotherInt2 = 107
+swapTwoValues(&someInt2, &anotherInt2)
+// someInt is now 107, and anotherInt is now 3
+
+var someString = "hello"
+var anotherString = "world"
+swapTwoValues(&someString, &anotherString)
+
+//MARK: GENERIC TYPES
+
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+
+let fromTheTop = stackOfStrings.pop()
+
+//MARK: EXTENSION
+
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+}
+
+if let topItem = stackOfStrings.topItem {
+    print("The top item on the stack is \(topItem).")
+}
+//MARK: CONSTRAINT SYNTAX
+//placing a single class or protocol constraint after a type parameter’s name, separated by a colon, as part of the type parameter list.
+
+//func someFunction<T: SomeClass, U: SomeProtocol>(someT: T, someU: U) {
+//    // function body goes here
+//}
+
+//func findIndex<T>(of valueToFind: T, in array:[T]) -> Int? {
+//    for (index, value) in array.enumerated() {
+//        if value == valueToFind {
+//            return index
+//        }
+//    }
+//    return nil
+//}
+//
+//not all types that use '==' to compare, but you can use the Equatable protocol
+func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
+let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
