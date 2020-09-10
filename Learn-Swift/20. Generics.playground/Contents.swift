a7d48b7d1968e6f2d2426fd2f1fd06e5186bade4
Gabriel Gonçalves <GGGguimaraesgabriel@hotmail.com>
2020-09-09 22:28:27 -0300

    Generics part 2

--- Learn-Swift/20. Generics.playground/Contents.swift
+++ Learn-Swift/20. Generics.playground/Contents.swift
@@ -103,3 +103,92 @@ func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
 
 let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
 let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
+
+
+//MARK: Associated Types
+//CREATE CONTAINER:
+//1. It must be possible to add a new item to the container with an append(_:) method.
+//2. It must be possible to access a count of the items in the container through a count property that returns an Int value.
+//3. It must be possible to retrieve each item in the container with a subscript that takes an Int index value.
+
+protocol Container {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+struct IntStack: Container {
+    // original IntStack implementation
+    var items = [Int]()
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+    // conformance to the Container protocol
+    typealias Item = Int
+    mutating func append(_ item: Int) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Int {
+        return items[i]
+    }
+}
+
+//OR
+struct Stack2<Element>: Container {
+    // original Stack<Element> implementation
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    // conformance to the Container protocol
+    mutating func append(_ item: Element) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
+
+extension Stack2 where Element: Equatable {
+    func isTop(_ item: Element) -> Bool {
+        guard let topItem = items.last else {
+            return false
+        }
+        return topItem == item
+    }
+}
+
+var stack = Stack2<Int>()
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+print(stack.items)
+
+var stack2 = Stack2<Int>()
+stack2.append(1)
+stack2.append(2)
+stack2.append(3)
+stack2.append(4)
+print(stack2.items)
+
+if stack.isTop(3) {
+    print("Top element is tres.")
+} else {
+    print("Top element is something else.")
+}
+
+
