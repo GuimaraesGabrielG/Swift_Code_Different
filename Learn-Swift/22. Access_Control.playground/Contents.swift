285f8e3b2ab68cc74deeea1429c5c5bdbb57dc33
Gabriel Gonçalves <GGGguimaraesgabriel@hotmail.com>
2020-09-21 20:59:21 -0300

    Access_Control

---
+++ Learn-Swift/22. Access_Control.playground/Contents.swift
@@ -0,0 +1,12 @@
+//MARK: Access Control - can assign specific access levels to individual types (classes, structures, and enumerations), as well as to properties, methods, initializers, and subscripts belonging to those types. Protocols can be restricted to a certain context, as can global constants, variables, and functions.
+
+//MARK: Access Levels
+//Swift provides five different access levels for entities within your code
+
+//-Open access and public
+
+//-Internal access
+
+//-File-private access
+
+//-Private access
